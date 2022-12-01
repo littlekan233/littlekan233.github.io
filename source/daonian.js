bbe72c6185bd6af8a5b39812276079b367782615
一个小阚阚 <75866787+littlekan233@users.noreply.github.com>
2022-12-01 11:01:47 +0800

    Update daonian.js

--- source/daonian.js
+++ source/daonian.js
@@ -1,15 +1,6 @@
 (function(){
-  var json;
-  var contentCSS = document.querySelector("html") | document.query;
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/daonianinfo.json", true);
-  xhr.send()
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      json = JSON.parse(xhr.responseText);
-    }
-  }
-  if(json.enable){
+  var enable=true;
+  if(enable){
     document.body.style.filter = "grayscale(1)";
   }
 })()
